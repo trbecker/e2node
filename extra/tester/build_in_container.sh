5ddbf7198e5918da4908dccc1dac262bfdd342a7
Thiago Rafael Becker <thiago.becker@gmail.com>
2022-11-03 15:10:27 -0300

    Add gcov and valgrind summaries.

--- extra/tester/build_in_container.sh
+++ extra/tester/build_in_container.sh
@@ -5,5 +5,7 @@ cmake /src -DTESTING=1
 make
 VALGRIND=y ./test_runner
 gcov -mpc $(find . -name \*.o)
+gcovr -r /src . > gcovr-summary.txt
+gcovr -r /src . --json > gcovr-full.json
 cp --parents $(find . -name \*.gcov) /results
-cp valgrind-result.xml /results
+cp valgrind-result.xml gcovr-summary.txt gcovr-full.json /results
